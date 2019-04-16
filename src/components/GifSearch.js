f9a259bfde9aed49e603a0021becb02451799aa7
Elise Phan <aliceconnic@gmail.com>
2019-04-16 16:16:06 +0000

    Automatically backed up by Learn

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react'
  
 class GifSearch extends Component {
  
+<<<<<<< HEAD
 }
 
 export default GifSearch
+=======
+  state = {
+    peopleInSpace: []
+  }
+ 
+  render() {
+    return (
+      <div>
+        {this.state.peopleInSpace.map(person => person.name)}
+      </div>
+    )
+  }
+ 
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
+  }
+}
+ 
+export default App
+>>>>>>> 6e6b49aa63d5d3e1d23eae750e903337ef75cc26
